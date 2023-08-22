255d29e80da9e7e5186390aa59bee2f94ad96064
SuryaXworkz18 <suryam.xworkz@gmail.com>
2023-08-22 12:04:34 +0530

    array examples

---
+++ SuperMarket.java
@@ -0,0 +1,17 @@
+class SuperMarket{
+static String Groceries[]={"Tide","Rin","oliveoil","coconutoil","cinthol soap","margo soap","rin soap","shampoo","axe scent","comb","toothpaste","tub","mug","nailcutter","wheatpacket"};
+static String Snacks[]={"hideandseek","treat","cheetos","goodday","mariegold","cocacola","sprite","7up","mirinda","thumpsup","lays","bingo","nachos","picknik","onionrings"};
+
+public static void main(String  args[]){
+System.out.println("The groceries which are at discounts are"+Groceries[0]+Groceries[1]+Groceries[2]+Groceries[3]+Groceries[4]+
+										Groceries[5]+Groceries[6]+Groceries[7]+Groceries[8]+Groceries[9]+
+										Groceries[10]+Groceries[11]+Groceries[12]+Groceries[13]+Groceries[14]);
+System.out.println("The  Snacks which are at discounts are"+Snacks[0]+Snacks[1]+Snacks[2]+Snacks[3]+Snacks[4]+
+										Snacks[5]+Snacks[6]+Snacks[7]+Snacks[8]+Snacks[9]+
+										Snacks[10]+Snacks[11]+Snacks[12]+Snacks[13]+Snacks[14]);
+										
+																				
+										
+										
+										}
+										}
