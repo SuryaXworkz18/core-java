cb5d3f5d44b3eed445df4569a2359ca9582e326b
SuryaXworkz18 <suryam.xworkz@gmail.com>
2023-08-22 10:56:43 +0530

    variable and datatype example

---
+++ AcCooler.java
@@ -0,0 +1,58 @@
+class AcCooler{
+                    static String brand = "Voltas";
+					static String modelName = "CU 193V ADJ/EU 193V ADJ";
+					static String type = "Split";
+					static String capacityInTons = "1.6 Ton";				
+					static String color = "White";
+					static String coolingCapcity = "5400W";
+					static String compressor = "Rotary";
+					static String dehumidification = "Yes";
+					static String remoteControl = "Yes";
+					static String condenserCoil = "Copper";
+					static String powerRequirement = "AC 230 V";
+
+		public static void main(String args[]){
+		
+					
+					/*String brand = "Voltas";
+					String modelName = "CU 193V ADJ/EU 193V ADJ";
+					String type = "Split";
+					String capacityInTons = "1.6 Ton";				
+					String color = "White";
+					String coolingCapcity = "5400W";
+					String compressor = "Rotary";
+					String dehumidification = "Yes";
+					String remoteControl = "Yes";
+					String condenserCoil = "Copper";
+					String powerRequirement = "AC 230 V";*/
+					
+					
+					
+								
+								System.out.println("brand is " + brand);
+								System.out.println("Model Name is " + modelName);
+								System.out.println("type is " + type);
+								System.out.println("Its Capacity Intons " + capacityInTons);
+								System.out.println("color is  " + color);
+								System.out.println("cooling Capcity is " + coolingCapcity);
+								System.out.println("Its Compression is " + compressor);
+								System.out.println(" Dehumidification is " + dehumidification);
+								System.out.println("Remote control is " + remoteControl);
+								System.out.println("Its Condenser Coil is " + condenserCoil);
+								System.out.println("Its Power Requirement" + powerRequirement);
+								
+
+		
+		
+		
+		}
+
+
+
+}
+
+
+
+
+
+
