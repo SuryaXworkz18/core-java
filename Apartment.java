de185b66d0211ef7a293422494682a0309e48506
SuryaXworkz18 <suryam.xworkz@gmail.com>
2023-08-29 13:02:26 +0530

    create object

---
+++ Apartment.java
@@ -0,0 +1,15 @@
+class Apartment{
+   int apartmentID;
+   String apartmentName;
+   String location;
+   int noOfFloors;
+   int noOfTwobhkFalts;
+   int noOfThreebhkFalts;
+  
+  public void provideAccomodation(){
+	  System.out.println("to provide large space for living or accomodation");
+	  
+	  
+  }
+
+}
